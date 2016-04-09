0307fd293abe6cfdaf80254e3f971bad24ade2c6
Angelos Kyriacou <angkyria@podilates.com>
2016-04-09 14:08:50 +0300

    Correction in appearance

--- hw1/src/ce325/hw1/SeamCarver.java
+++ hw1/src/ce325/hw1/SeamCarver.java
@@ -1,20 +1,43 @@
+
+import java.util.Scanner;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
+import java.io.IOException;
 
-public class SeamCarver{
 
-    String nameImage;
-    /*File file;
-    URL url;
-    BufferedImage image;
-    */
-    public SeamCarver(String name) {
+public class SeamCarver {
 
-        nameImage = name;
-        System.out.println("hi iam the "+nameImage);
 
+    /*Constructors*/
+
+    public SeamCarver(BufferedImage image) throws IOException {
 
     }
 
+    public SeamCarver(File file) throws IOException {
+
+    }
+
+
+    public SeamCarver(URL url) throws IOException {
+
+    }
+
+
+
+    public static void main(String[] args){
+        
+        String image_name;
+
+        if (args.length>0){
+            image_name = args[0];
+        }else{
+            System.out.println("Please give image name or url: ");
+            Scanner scanIn = new Scanner(System.in);
+            image_name = scanIn.nextLine(); 
+        } 
+
+
+    }
 }
